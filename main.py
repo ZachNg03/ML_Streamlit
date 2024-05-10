7e5f214280e7556cb2bdc4ffe328787d8fa1d85e
ZachNg03 <111360360+ZachNg03@users.noreply.github.com>
2024-05-10 22:51:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-import folium
-from folium.plugins import MarkerCluster
 from sklearn.manifold import TSNE
 from sklearn.cluster import AgglomerativeClustering
 from sklearn_extra.cluster import KMedoids
